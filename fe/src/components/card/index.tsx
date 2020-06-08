0727c46940c93de636ccbb1ba3e7184ee792b3f3
jaewonYoon <bunggl@naver.com>
2020-06-08 18:24:36 +0900

    building CardComponent

---
+++ fe/src/components/card/index.tsx
@@ -0,0 +1,65 @@
+import React, { FC } from 'react';
+import { CardData } from 'src/models/card';
+import * as Styled from './style';
+
+const { 
+  Container,
+  ImgBox,
+  ContentBox,
+  CategoryBox,
+  CategoryIcon,
+  Category,
+  MemberCount,
+  TitleBox,
+  Title,
+  TitleIcons,
+  IconBox
+} = Styled; 
+
+interface Props {
+  width?: number,
+  height?: number, 
+  data: CardData
+}
+
+export const Card:FC<Props> = ({
+  width,
+  height,
+  data
+}) => {
+  const { 
+    imgUrl,
+    title,
+    description,
+    host, 
+    members,
+    full, 
+    current,
+    category,
+    url
+  }  = data; 
+  const extractedUrl = imgUrl ?? 'none'; 
+  return(
+    <>
+      <Container>
+        <ImgBox imgUrl={extractedUrl}/>
+      </Container>
+      <ContentBox>
+        <CategoryBox>
+          <CategoryIcon />
+          <Category>{category}</Category>
+          <MemberCount> {current} / {full} </MemberCount>  <MemberCount/>  
+        </CategoryBox>
+        <TitleBox>
+          <Title>
+            {title}
+          </Title>
+          <TitleIcons>
+            <IconBox /> 
+            <IconBox />
+          </TitleIcons>        
+        </TitleBox>
+      </ContentBox>  
+    </>
+  )
+}
