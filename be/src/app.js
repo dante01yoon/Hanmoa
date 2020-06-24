e47b455d70bc5fb6b33d44bbb2e68b85ce288c2a
risel <bunggl@naver.com>
2020-06-24 22:07:10 +0900

    mongodb config edit

--- be/src/app.js
+++ be/src/app.js
@@ -1,7 +1,7 @@
+
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
-
 const authRoutes = require('./routes/auth-routes');
 const mongoConnect = require('./config/mongoConfig');
 
--- be/src/config/mongoConfig.js
+++ be/src/config/mongoConfig.js
@@ -6,7 +6,9 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Connect to mongodb server
-const getMongoConnect = () => mongoose.connect(process.env.MONGODB_URI)
+const getMongoConnect = () => mongoose.connect(process.env.MONGODB_URI, { 
+    useUnifiedTopology: true, useNewUrlParser: true 
+  })
   .then(()=> console.log('Successfully connected to mongodb'))
   .catch(e => console.error(e));
 
