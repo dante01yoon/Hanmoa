da131a134fcba673a056c0894563811018a8ff0e
dante01yoon <bunggl@naver.com>
2021-06-25 23:32:01 +0900

    Update: toast 설계중

--- fe/src/store/toast.tsx
+++ fe/src/store/toast.tsx
@@ -1,10 +1,13 @@
 
-import React, { createContext } from "react";
-import styled from "styled-components";
-import { observable } from "mobx";
+import React from "react";
+import styled, { css } from "styled-components";
 import { useLocalStore, } from "mobx-react-lite";
 
 const StyledBlackContainer = styled.div`
+  position: fixed;
+  z-index: 1050;
+  width: 100%;
+  height: 100%;
 `;
 
 const StyledToastWrapper = styled.div`
@@ -41,17 +44,37 @@ const StyledToastTopContainer = styled.div`
   transform: translateX(-50%);
 `;
 
+const StyledSingleToastWrapper = styled.div<{
+  nth: number;
+  position: string;
+}>`
+  position: absolute;
+  transition: all ease 0.3s;
+  ${({ position, nth }) => css`${position}: ${(nth * 50)}px`}; 
+`;
+
+type ToastPosition = "top" | "right" | "bottom" | "left";
+
+interface OpenToastOptions {
+  position: ToastPosition;
+  hasBlackBackgroud?: boolean;
+}
+
+interface ToastElement {
+  toastElement: React.ReactElement;
+  position: ToastPosition;
+}
+
 const createToastManager = () => {
-  const toasts: Array<React.ReactElement> = [];
+  const toasts: Array<ToastElement> = [];
+
+  const openToast = (toast: ToastElement, options: OpenToastOptions) => {
+    toasts.push(toast);
+  }
 
   return useLocalStore(() => ({
     toasts,
-    closeToast: () => {
-      toasts.pop();
-    },
-    openToast: (toast: React.ReactElement) => {
-      toasts.push(toast);
-    }
+    openToast,
   }))
 }
 
@@ -66,28 +89,38 @@ const ToastContext = React.createContext<ToastStore | null>(null);
 const ToastManager: React.FC<ToastManagerProps> = ({
   value,
 }) => {
+
+  const handleToastTransition = (toast, index) => {
+    return (
+      <StyledSingleToastWrapper>
+
+      </StyledSingleToastWrapper>
+    )
+  }
+
+  const renderToasts = (toasts: ToastElement[], position: ToastPosition) => {
+    return React.Children.toArray(
+      toasts.filter((toast) => toast.position === position)
+        .map((filteredToast) => React.cloneElement(filteredToast.toastElement))
+    )
+  }
+
+
   return (
     <ToastContext.Provider value={value}>
       <StyledToastWrapper>
         <StyledToastTopContainer>
-
+          {renderToasts(value.toasts, "top")}
         </StyledToastTopContainer>
         <StyledToastRightContainer>
-
+          {renderToasts(value.toasts, "right")}
         </StyledToastRightContainer>
         <StyledToastBottomContainer>
-
+          {renderToasts(value.toasts, "bottom")}
         </StyledToastBottomContainer>
         <StyledToastLeftContainer>
-
+          {renderToasts(value.toasts, "left")}
         </StyledToastLeftContainer>
-        {
-          React.Children.toArray(
-            value.toasts.map((element) =>
-              React.cloneElement(element)
-            )
-          )
-        }
       </StyledToastWrapper>
     </ToastContext.Provider>
   )
