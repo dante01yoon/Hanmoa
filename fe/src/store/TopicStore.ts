e3a79c1aceab5c42bfaeb656b71dffd212c01d8d
dante01yoon <bunggl@naver.com>
2021-07-07 20:59:47 +0900

    Update: TopicStore v6 migration

--- fe/src/store/TopicStore.ts
+++ fe/src/store/TopicStore.ts
@@ -1,28 +1,31 @@
 import BasicStore from "@store/BasicStore";
-import { action, observable, makeObservable} from "mobx";
+import { action, observable, makeObservable } from "mobx";
 import RootStore from "@store/RootStore";
 import { GetTopicListPayload } from "@payload/index";
 
-class TopicStore extends BasicStore{
-  @observable topicList: GetTopicListPayload["topicList"];
-  constructor({root, state}: {root: RootStore, state: TopicStore}) {
-    super({root});
-    makeObservable(this);
+class TopicStore extends BasicStore {
+  topicList: GetTopicListPayload["topicList"] = [];
+
+  constructor({ root, state }: { root: RootStore, state: TopicStore }) {
+    super({ root });
+    makeObservable(this, {
+      topicList: observable
+    });
     this.topicList = state?.topicList ?? [];
   }
-  
+
   async fetchTopicList() {
     const [error, response] = await this.api.GET<GetTopicListPayload>("/topic/list")
-    if(response){
+    if (response) {
       this.update(response.data);
     }
-    if(error){
+    if (error) {
       throw Error(error.error);
-    }    
+    }
   }
 
   @action
-  update({topicList}: GetTopicListPayload){
+  update({ topicList }: GetTopicListPayload) {
     this.topicList = topicList;
   }
 }
